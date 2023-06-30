4ba4c890ad4bb962d263a639a41c0c6e851e2a76
lorne <emailyuliang@126.com>
2023-06-30 15:01:22 +0800

    remove test

--- demo4.3/oa/src/main/java/com/example/oa/controller/OaController.java
+++ demo4.3/oa/src/main/java/com/example/oa/controller/OaController.java
@@ -1,28 +1,33 @@
 package com.example.oa.controller;
 
-import com.codingapi.crankycode.kafka.event.KafkaPusher;
-import com.codingapi.crankycode.kafka.message.TestMessage;
 import com.example.oa.controller.pojo.ApproveCommand;
 import com.example.oa.model.FsProcessApprovalService;
 import com.example.oa.model.domain.FsProcess;
 import com.example.oa.model.domain.User;
 import com.example.oa.model.repository.FsProcessRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/oa")
 @AllArgsConstructor
 public class OaController {
 
-    private FsProcessRepository processRepository;
+    private final FsProcessRepository processRepository;
+
+    private final FsProcessApprovalService approvalService = new FsProcessApprovalService();
 
     @PostMapping("/createProcess")
     public boolean createProcess(@RequestBody ApproveCommand.Create command) {
         User user = new User(command.getUsername());
-        FsProcess fsProcess = new FsProcess(command.getProcessName(), user, command.getMoney());
         User appoverUser = new User(command.getApproverUser());
+
+        FsProcess fsProcess = new FsProcess(command.getProcessName(), user, command.getMoney());
         fsProcess.setApproverUser(appoverUser);
+
         processRepository.save(fsProcess);
         return true;
     }
@@ -32,16 +37,9 @@ public class OaController {
     public boolean approve(@RequestBody ApproveCommand.Approve command) {
         User approverUser = new User(command.getApproverUser());
         FsProcess fsProcess =  processRepository.getFsProcessByName(command.getProcessName());
-        FsProcessApprovalService approvalService = new FsProcessApprovalService(approverUser,fsProcess);
-        approvalService.approve(command.isApproved());
+        approvalService.approve(approverUser,fsProcess,command.isApproved());
         return true;
     }
 
-    @GetMapping("/test")
-    public void test(){
-        TestMessage testMessage = new TestMessage();
-        testMessage.setMsg("hello");
-        KafkaPusher.push(testMessage);
-    }
 
 }
