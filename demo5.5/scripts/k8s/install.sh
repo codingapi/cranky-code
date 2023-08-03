2059cccdd4dcd1ef446275df021d282fd91aa082
lorne <1991wangliang@gmail.com>
2023-08-03 20:40:18 +0800

    add k8s script

--- demo5.5/scripts/k8s/install.sh
+++ demo5.5/scripts/k8s/install.sh
@@ -1,7 +1,7 @@
 kubectl apply -f server-deployment.yaml
-kubectl apply -f server-sevice.yaml
+kubectl apply -f server-service.yaml
 
 kubectl apply -f monitor-deployment.yaml
-kubectl apply -f monitor-sevice.yaml
+kubectl apply -f monitor-service.yaml
 
 kubectl get pods,services
