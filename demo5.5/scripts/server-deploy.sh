e2af4bff4851c10ddab65e7188fcd8656960ceff
lorne <1991wangliang@gmail.com>
2023-08-03 19:09:36 +0800

    fix bug

--- demo5.5/scripts/server-deploy.sh
+++ demo5.5/scripts/server-deploy.sh
@@ -3,7 +3,7 @@
 # 定义版本号
 VERSION=1.0.0
 # 镜像仓库地址
-REGISTRY-URL=10.13.14.230:5000
+REGISTRY_URL=10.13.14.230:5000
 
 # 构建 Docker 镜像
 docker build -t server:$VERSION -f ./server.Dockerfile .
@@ -12,6 +12,6 @@ docker build -t server:$VERSION -f ./server.Dockerfile .
 docker tag server:$VERSION $REGISTRY-URL/server:$VERSION
 
 # 推送镜像到私有 Registry，包括版本号
-docker push $REGISTRY-URL/server:$VERSION
+docker push $REGISTRY_URL/server:$VERSION
 
-curl -X GET http://$VERSION/v2/server/tags/list
+curl -X GET http://$REGISTRY_URL/v2/server/tags/list
